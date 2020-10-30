eea734877bc3013cf063f8a4fca56d9833871cdb
spagnon7 <spagnon7@gmail.com>
2020-10-29 22:11:55 -0700

    drafted all js except greying buttons

--- app.js
+++ app.js
@@ -3,19 +3,49 @@ const p2Score = document.querySelector('.p2-score');
 
 const p1Plus = document.querySelector('.p1-plus');
 const p2Plus = document.querySelector('.p2-plus');
+const reset = document.querySelector('.reset');
 
-//Setting P1/P2 buttons to increment score
+//Setting reset button to reset score/colors
+
+reset.addEventListener('click', () => {
+    p1Score.innerText = 0;
+    p2Score.innerText = 0;
+
+    p1Score.style.color = '#6930c3';
+    p2Score.style.color = '#6930c3';
+})
+
+//Setting P1/P2 button scoring logic
+
+const select = document.querySelector('#win-score');
 
 p1Plus.addEventListener('click', function() {
+    let winScore = select.value;
+    
     let currentScore = parseInt(p1Score.innerText);
     let newScoreNum = currentScore + 1;
     let newScoreStr = newScoreNum.toString();
     p1Score.innerText = newScoreStr;
+
+    console.log(newScoreStr);
+    console.log(winScore);
+
+    if (newScoreStr === winScore) {
+        p1Score.style.color = 'green';
+        p2Score.style.color = 'red';
+    }
 })
 
 p2Plus.addEventListener('click', function() {
+    let winScore = select.value;
+    
     let currentScore = parseInt(p2Score.innerText);
     let newScoreNum = currentScore + 1;
     let newScoreStr = newScoreNum.toString();
     p2Score.innerText = newScoreStr;
+
+    if (newScoreStr === winScore) {
+        p2Score.style.color = 'green';
+        p1Score.style.color = 'red';
+    }
 })
